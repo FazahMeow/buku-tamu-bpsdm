b16f4f40f11e93bb6fa4f25b34c2f612572f8f08
fahazzs <ElyassVA05@gmail.com>
2024-09-12 09:33:19 +0700

    routing, filling create model

--- resources/views/bukuTamu/create.blade.php
+++ resources/views/bukuTamu/create.blade.php
@@ -7,5 +7,34 @@
 </head>
 <body>
     <h1>Fill this form</h1>
+    <form method="post" action="">
+        <div>
+            <label>Nama</label>
+            <input type="text" name="nama" placeholder="Isi nama lengkap"/>
+        </div>
+        <div>
+            <label>No HP</label>
+            <input type="text" name="nohp" placeholder="Isi nomor HP aktif"/>
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="text" name="email" placeholder="Isi Email"/>
+        </div>
+        <div>
+            <label>Instansi</label>
+            <input type="text" name="instansi" placeholder="Isi instansi"/>
+        </div>
+        <div>
+            <label>Tujuan</label>
+            <input type="text" name="tujuan" placeholder="Isi tujuan"/>
+        </div>
+        <div>
+            <label>Keterangan</label>
+            <input type="text" name="keterangan" placeholder="Keterangan..."/>
+        </div>
+        <div>
+            <input type="submit" value="Kirim"/>
+        </div>
+    </form>
 </body>
 </html>
